c82931a0eb6ba10d58f0e72b6185b26521870e8a
Kostiantyn <kostiantyn.shevtsov@gmail.com>
2022-09-11 14:40:42 +0200

    front html js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// let select = document.getElementById('select');
+
+let sort = document.querySelector('#sort');
+sort.addEventListener('change',function(){
+    console.log(sort.value);
+});
+// input.addEventListener('click',function(){
+//     select.value = 2;
+//     select.dispatchEvent(new Event('change'));
+// });
